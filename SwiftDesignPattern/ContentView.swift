7ae503014fa52d9a566df875cd54544ae76a9391
Uwais Alqadri <uwaisalqadri654321@gmail.com>
2024-09-15 07:46:20 +0700

    Initial Commit

---
+++ SwiftDesignPattern/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftDesignPattern
+//
+//  Created by Aleph-WQ05D on 15/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
